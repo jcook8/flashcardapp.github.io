e49942f996ba3c01ce22d8bbb65424eb86ba0abe
jcook8 <jpianohands@gmail.com>
2017-07-25 11:59:52 -0400

    changes

--- main.py
+++ main.py
@@ -35,3 +35,11 @@ app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/saved', SavedHandler)
 ], debug=True)
+
+
+
+word = { "status": 200, "offset": 0, "limit": 1, "count": 1,  "total": 1,
+  "url": "/v2/dictionaries/ldoce5/entries?headword=hypochondriac&limit=1",
+  "results": [{"datasets": ["ldoce5","dictionary"],"headword": "hypochondriac", "id": "cqAFNmnynQ","part_of_speech": "noun","pronunciations": [{"audio": [{"lang": "British English","type": "pronunciation","url": "/v2/dictionaries/assets/ldoce/gb_pron/hypochondriac0205.mp3" }],
+  "ipa": "ˌhaɪpəˈkɒndriæk"}, { "audio": [{ "lang": "American English", "type": "pronunciation", "url": "/v2/dictionaries/assets/ldoce/us_pron/hypochondriac.mp3" }], "ipa": "-ˈkɑːn-", "lang": "American English" }],
+  "senses": [{ "definition": ["someone who always worries about their health and thinks they may be ill, even when they are really not ill"] }], "url": "/v2/dictionaries/entries/cqAFNmnynQ" }]}
